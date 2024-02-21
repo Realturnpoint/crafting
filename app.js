26ed0ac11a3cc5a9dc7e16ba0a81926ccd5ac8ac
turnpoint <realturnpoint@gmail.com>
2024-02-21 18:47:03 +0100

    Version Alpha 1.0

--- app.js
+++ app.js
@@ -18,7 +18,26 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("material12"),
         document.getElementById("material13"),
         document.getElementById("material14"),
-        document.getElementById("material15")
+        document.getElementById("material15"),
+        document.getElementById("material16"),
+        document.getElementById("material17"),
+        document.getElementById("material18"),
+        document.getElementById("material19"),
+        document.getElementById("material20"),
+        document.getElementById("material21"),
+        document.getElementById("material22"),
+        document.getElementById("material23"),
+        document.getElementById("material24"),
+        document.getElementById("material25"),
+        document.getElementById("material26"),
+        document.getElementById("material27"),
+        document.getElementById("material28"),
+        document.getElementById("material29"),
+        document.getElementById("material30"),
+        document.getElementById("material31"),
+        document.getElementById("material32"),
+        document.getElementById("material33"),
+        document.getElementById("material34")
     ];
 
     // Database of items
@@ -88,6 +107,132 @@ document.addEventListener("DOMContentLoaded", function() {
             material7: 15,
             material15: 15
         },
+        {
+            name: "Grijze USB",
+            material14: 1,
+            material2: 3,
+            material9: 1
+        },
+        {
+            name: "Gereedschapsset",
+            material16: 2,
+            material2: 5,
+            material5: 3
+        },
+        {
+            name: "Slakken Fossiel",
+            material17: 5,
+        },
+        {
+            name: "Leeg zakje",
+            material18: 1,
+        },
+        {
+            name: "Versteende Slak",
+            material17: 5,
+        },
+        {
+            name: "Trilobiet Fossiel",
+            material17: 5,
+        },
+        {
+            name: "Tas",
+            material19: 15,
+            material20: 5
+        },
+        {
+            name: "Vpn",
+            material15: 5,
+            material14: 1,
+            material21: 2
+        },
+        {
+            name: "GPS Verwijderaar",
+            material2: 10,
+            material9: 10,
+            material7: 15,
+            material15: 5,
+            material22: 2
+        },
+        {
+            name: "Kogelwerend Vest",
+            material20: 5,
+            material19: 10
+        },
+        {
+            name: "MijnLaser",
+            material23: 1,
+            material24: 1,
+            material2: 10,
+            material9: 15
+        },
+        {
+            name: "Zware Boor",
+            material2: 10,
+            material24: 1,
+            material8: 15,
+            material23: 1
+        },
+        {
+            name: "Hacking Device",
+            material22: 1,
+            material1: 10,
+            material14: 1,
+            material9: 10,
+            material15: 5,
+        },
+        {
+            name: "Pijpbom",
+            material2: 5,
+            material25: 2,
+            material26: 1,
+            material3: 10,
+            material27: 2
+        },
+        {
+            name: "Mal voor Clip",
+            material2: 20,
+            material28: 1
+        },
+        {
+            name: "Mal voor Trigger",
+            material2: 20,
+            material28: 1
+        },
+        {
+            name: "Mal voor Loop",
+            material2: 20,
+            material28: 1
+        },
+        {
+            name: "9mm Ammo Box",
+            material29: 5,
+            material5: 5,
+            material30: 5
+        },
+        {
+            name: "Mal voor Extended Clip",
+            material2: 20,
+            material28: 1
+        },
+        {
+            name: "Mal voor Silencer",
+            material2: 20,
+            material28: 1
+        },
+        {
+            name: "SNS Pistool",
+            material31: 1,
+            material32: 1,
+            material33: 1,
+            material34: 5,
+            material1: 10
+        },
+        {
+            name: "Mal voor CombatPistool loop",
+            material2: 20,
+            material28: 1
+        },
     ];
 
     // Function to update item details
@@ -130,7 +275,45 @@ document.addEventListener("DOMContentLoaded", function() {
             case 14:
                 return "Printplaat";
             case 15:
-                return "koper";
+                return "Koper";
+            case 16:
+                return "Geavanceerde Lockpick";
+            case 17:
+                return "Stukje fossiel";
+            case 18:
+                return "Plastic Pulp";
+            case 19:
+                return "Stof";
+            case 20:
+                return "Rubber";
+            case 21:
+                return "USB";
+            case 22:
+                return "Antenne";
+            case 23:
+                return "Mijnboor";
+            case 24:
+                return "Elektronische kit";
+            case 25:
+                return "Aluminium Poeder";
+            case 26:
+                return "Pijpbom Huls";
+            case 27:
+                return "Ijzer Poeder";
+            case 28:
+                return "Houten bak Zand";
+            case 29:
+                return "9mm Hulzen";
+            case 30:
+                return "Buskruit";
+            case 31:
+                return "SNS Trigger";
+            case 32:
+                return "SNS Loop";
+            case 33:
+                return "SNS Clip";
+            case 34:
+                return "Aluminium";
             default:
                 return "";
         }
